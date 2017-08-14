8918ce19c2505dad4dd2bf32cd14032ce6feab12
daniel-certa-1228 <mfdancerta@gmail.com>
2017-08-14 09:14:46 -0500

    More setup

---
+++ javascripts/mercury.js
@@ -0,0 +1,26 @@
+"use strict";
+
+let mercury = {
+	name: "Mercury",
+	discovered: "1400BC",
+	mass:"3.3011×1023 kg",
+	size: "7.48×107 km2",
+	distance_from_sun: "35.98 million miles",
+	atmosphere: "hydrogen, helium, and oxygen",
+	satellites: "none",
+	visitors: "Mariner 10 (1974), MESSENGER (2004)"
+};
+
+function outputTo(domElement) {
+  domElement.append();
+}
+
+
+// Name
+// Year discovered
+// Mass
+// Size
+// Distance from Sun
+// Atmosphere composition
+// Satellites
+// Name of any probes/orbiters/explorers that have visited or landed on that plane
